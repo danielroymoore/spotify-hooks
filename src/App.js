77a9527d623e59f3570ce82fce0d62dee06f15dd
Daniel Moore <daniel@danielroymoore.com>
2019-01-23 01:08:24 +0000

    Initial auth process for api complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { navigate } from "@reach/router";
 
 class App extends Component {
+  state = {
+    token: null
+  };
+
+  beginAuth() {
+    let authUrl = `https://accounts.spotify.com/authorize?client_id=${
+      process.env.REACT_APP_CLIENT_ID
+    }&redirect_uri=${
+      process.env.REACT_APP_SPOTIFY_CALLBACK
+    }&response_type=token`;
+
+    navigate(authUrl);
+  }
+
+  componentDidMount() {
+    let returnValues = window.location.hash.substr(1);
+    this.setState({
+      token: returnValues.split("&")[0].split("=")[1]
+    });
+  }
+
   render() {
+    const { token } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <h1>Please Authorise Spotify</h1>
+        <button onClick={this.beginAuth}>Click Here</button>
+        <h1>{token}</h1>
+      </React.Fragment>
     );
   }
 }
